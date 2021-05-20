eca6da1ad2292bcbc7c6e5337e82a375992b26ca
zetmask <18582593+Zetmask@users.noreply.github.com>
2021-05-20 18:54:30 +0300

    temporary changes

--- src/main/java/com/autoSchoolPartThreeAndSix/Main.java
+++ src/main/java/com/autoSchoolPartThreeAndSix/Main.java
@@ -1,59 +1,77 @@
 package com.autoSchoolPartThreeAndSix;
-import java.util.Map;
 import java.util.TreeMap;
-import java.util.Collections;
 import java.util.*;
-
-import com.autoSchoolPartThreeAndSix.figure.Circle;
-import com.autoSchoolPartThreeAndSix.figure.Figures;
-import com.autoSchoolPartThreeAndSix.figure.Square;
-import com.autoSchoolPartThreeAndSix.figure.Triangle;
+import com.autoSchoolPartThreeAndSix.figure.*;
 
 public class Main {
 
 
     public static void main(String[] args) {
 
+        sortFigures(createArrayOfRandomFigures(10));
 
-        createRandomFigures(10)
+    }
 
-        public static Figures createRandomFigure(int numbers) {
-        Figures[] list = new Figures[numbers];
-        for (int i = 0; i < list.length; i++) {
+
+
+ public static int[] createArrayOfRandomCoordinates(int points) {
+
+        int[] arrayOfCoordinates = new int[points];
+        Random random = new Random();
+        for (int i = 0; i < arrayOfCoordinates.length; i++) {
+            arrayOfCoordinates[i] = random.nextInt(50 + 1);
+        }
+        return arrayOfCoordinates;
+    }
+
+        public static Figures[] createArrayOfRandomFigures(int numbers) {
+        Figures[] arrayOfRandomFigures = new Figures[numbers];
+        for (int i = 0; i < arrayOfRandomFigures.length; i++) {
             int a = 2 + (int) (Math.random() * 3);
             if (a == 2) {
-                list[i] = new Square();
+                arrayOfRandomFigures[i] = new Square("Square", createArrayOfRandomCoordinates(4), createArrayOfRandomCoordinates(4));
             }
             if (a == 3) {
-                list[i] = new Circle();
+                arrayOfRandomFigures[i] = new Square("Circle", createArrayOfRandomCoordinates(2), createArrayOfRandomCoordinates(2));
             }
             if (a == 4) {
-                list[i] = new Triangle();
+                arrayOfRandomFigures[i] = new Square("Triangle",createArrayOfRandomCoordinates(3), createArrayOfRandomCoordinates(3));
             }
-            return list;
         }
-           // System.out.println(list[i]);
+            return arrayOfRandomFigures;
+    }
 
-    sortFigures(int[] unsortedFigures){
-            TreeMap<Double, String> sortedArea = new TreeMap<Double, String>();
+    public static void sortFigures(Figures[] arrayOfRandomFigures) {
+    Map<Double, String> mapWithFigures = new TreeMap<Double, String>();
+        for (Figures currentFigure : arrayOfRandomFigures) {
+        double area = (currentFigure).countedArea();
+        mapWithFigures.put(area, currentFigure.getName());
+    }
+        for (Map.Entry<Double, String> entry : mapWithFigures.entrySet()) {
+        System.out.println(entry.getValue() + ", S = " + entry.getKey());
+    }
+}
 
-            sortedArea.put(list[0].getCountedArea(), list[0].getName());
-            sortedArea.put(list[1].getCountedArea(), list[1].getName());
-            sortedArea.put(list[2].getCountedArea(), list[2].getName());
-            sortedArea.put(list[3].getCountedArea(), list[3].getName());
-            sortedArea.put(list[4].getCountedArea(), list[4].getName());
-            sortedArea.put(list[5].getCountedArea(), list[5].getName());
-            sortedArea.put(list[6].getCountedArea(), list[6].getName());
-            sortedArea.put(list[7].getCountedArea(), list[7].getName());
-            sortedArea.put(list[8].getCountedArea(), list[8].getName());
-            sortedArea.put(list[9].getCountedArea(), list[9].getName());
-
-            for (Map.Entry m : sortedArea.entrySet()) {
-                System.out.println(m.getValue() + " " + m.getKey());
-            }
-            return int[] sortedFigures;
-        }
+//    public static void createTreeMapOfFigures() {
+//            TreeMap<Double, String> sortedArea = new TreeMap<Double, String>();
+//
+//            sortedArea.put(list[0].getArea(), list[0].getName());
+//            sortedArea.put(list[1].getArea(), list[1].getName());
+//            sortedArea.put(list[2].getArea(), list[2].getName());
+//            sortedArea.put(list[3].getArea(), list[3].getName());
+//            sortedArea.put(list[4].getArea(), list[4].getName());
+//            sortedArea.put(list[5].getArea(), list[5].getName());
+//            sortedArea.put(list[6].getArea(), list[6].getName());
+//            sortedArea.put(list[7].getArea(), list[7].getName());
+//            sortedArea.put(list[8].getArea(), list[8].getName());
+//            sortedArea.put(list[9].getArea(), list[9].getName());
+//
+////            for (Map.Entry m : sortedArea.entrySet()) {
+////                System.out.println(m.getValue() + " " + m.getKey());
+////            }
+//            System.out.println(sortedArea);
+//        }
 
 
     }
-}
+
